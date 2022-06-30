a704b4c3a0d1f0d16088a15a4d39d97050f73330
rohnshrma <rohn11shrma@gmail.com>
2022-06-30 19:09:38 +0530

    variables and data types

---
+++ script.js
@@ -0,0 +1,65 @@
+// alert
+// alert(1+2)
+// window.alert("hello world")
+
+// console ✔
+// console.log("hello world")
+// console.log("hello world")
+// console.log("hello world")
+// console.log("hello world")
+
+
+// ❌
+// document.write("hello world")
+
+// input
+
+// prompt("enter your name : " , "enter name here")
+// prompt("enter your name : ")
+
+
+// varibales
+// variables / contants
+// var / const 
+
+// let / const
+
+
+
+
+// var myName; //define
+//  myName = "rohan"; //value assign
+
+let myName = "rohan"; //declared and assigned value
+myName = "divyanshi"; // re-assigned
+
+// alert( typeof myName)
+
+
+const myAge = 24; // declared and assigned value
+//  myAge = 25; //re-assignment not possible with const
+
+// alert(typeof myAge)
+//  alert(typeof myAge)
+
+
+// alert(myAge)
+
+
+// data types
+// string => "" / ''
+
+console.log(typeof '2345')
+console.log(typeof 1.23)
+console.log(typeof true)
+console.log(typeof false)
+
+
+// arrays / objects
+
+
+
+
+// strings
+var yourName = "Rohan";
+
