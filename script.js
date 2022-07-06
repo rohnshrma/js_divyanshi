2e68f1adb8c3785e3fc3c47409c6bc424673827f
rohnshrma <rohn11shrma@gmail.com>
2022-07-06 18:15:40 +0530

    control flow statements

--- script.js
+++ script.js
@@ -1,65 +1,106 @@
-// alert
-// alert(1+2)
-// window.alert("hello world")
+// input / outputs
 
-// console ✔
-// console.log("hello world")
-// console.log("hello world")
-// console.log("hello world")
+// output
+// window.alert(1234  + 4321)
+// alert("helllo world")
+
 // console.log("hello world")
 
-
-// ❌
 // document.write("hello world")
 
-// input
+// containers
+// var  / const
 
-// prompt("enter your name : " , "enter name here")
-// prompt("enter your name : ")
-
-
-// varibales
-// variables / contants
-// var / const 
+// es6
 
 // let / const
 
+// var myName; // defining variable
+// myName = "rohan sharma" // assignment
 
+// const myAge = 34;
+// myAge = 33;
+// console.log(myAge)
 
+// variable naming conventions
 
-// var myName; //define
-//  myName = "rohan"; //value assign
+// var myName = "rohan sharma";
 
-let myName = "rohan"; //declared and assigned value
-myName = "divyanshi"; // re-assigned
+//           label            placeholder
+// prompt("enter your name : " , "name here")
+// the value of prompt will always be a string
 
-// alert( typeof myName)
+// var myName = prompt("enter Your Name", "Name goes here")
 
-
-const myAge = 24; // declared and assigned value
-//  myAge = 25; //re-assignment not possible with const
-
-// alert(typeof myAge)
-//  alert(typeof myAge)
-
-
-// alert(myAge)
-
-
-// data types
-// string => "" / ''
-
-console.log(typeof '2345')
-console.log(typeof 1.23)
-console.log(typeof true)
-console.log(typeof false)
-
-
-// arrays / objects
-
-
-
+// console.log(typeof myName)
 
 // strings
-var yourName = "Rohan";
+// "" / '' will be considered as string
+//  console.log(typeof "234");
+
+var myName = "rohan sharma is a student";
+// console.log(myName.length);
+// console.log(myName.toUpperCase());
+// console.log(myName);
+// console.log(myName.includes("a"));
+// console.log(myName.endsWith("a"));
+// console.log(myName.replace("r", "s"))
+// console.log(myName);
+// console.log(myName.slice(0,6));
+// console.log(myName.split("s"));
+
+
+// console.log(12+12);
+// console.log("12"+"12");
+
+
+// var fName = prompt("enter your First Name : ")
+// var lName = prompt("enter your Last Name : ")
+
+// console.log(fName + " " + lName + " is a website developer")
+
+
+// string literals / template literals
+// console.log(`${fName} ${lName} is a website develope.`);
+
+// arithematic operators
+var a = 20;
+var b = 3;
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// console.log(a**2)
+// console.log(++a)
+// console.log(--a)
+
+// control flow statement
+// if(a == 21 ){
+//     alert("correct")
+// }else{
+//     alert("incorrect")
+// }
+
+// var age= Number(prompt("enter age", "age"))
+// console.log(typeof age)
+
+
+// if(age===18)
+// {
+//     alert("you are elegible for vote")
+// }
+// else{
+//     alert("you are not elegible for vote")
+// }
+
+
+// if (age > 60){
+//     alert("not fit to vote")
+// }
+// else if (age > 18){
+//     alert("eligible to vote")
+// } else{
+//     alert("not eligible to vote")
+// }
 
